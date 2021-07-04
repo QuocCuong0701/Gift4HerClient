186e969316b8c40174d980b6e7b23f90019a13c0
QuocCuong0701 <quoc.cuong.yb.dhtb@gmail.com>
2021-07-04 21:56:32 +0700

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <Index/>
+        <LoaderWrapper/>
+    </div>
+</template>
+
+<script>
+    import Index from '@/components/Index.vue'
+    import LoaderWrapper from "@/components/LoaderWrapper";
+
+    export default {
+        name: 'App',
+        components: {
+            LoaderWrapper,
+            Index
+        }
+    }
+</script>
+
+<style>
+    @import "assets/css/bootstrap.min.css";
+    @import "assets/css/hero-slider-style.css";
+    @import "assets/css/magnific-popup.css";
+    @import "assets/css/templatemo-style.css";
+    @import "assets/css/style.css";
+    @import "assets/Font-Awesome-4.7/css/font-awesome.min.css";
+    @import "//fonts.googleapis.com/css?family=Open+Sans: 300,400,600";
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        /*margin-top: 60px;*/
+    }
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,244 @@
+<template>
+    <!-- Content -->
+    <div class="cd-hero">
+        <!-- Navigation -->
+        <div class="cd-slider-nav">
+            <nav class="navbar">
+                <div class="tm-navbar-bg">
+                    <!--<a class="navbar-brand text-uppercase" v-bind:href = "clientDomain"><i class="fa fa-picture-o tm-brand-icon"></i>Gallery</a>-->
+                    <a class="navbar-brand text-uppercase" v-bind:href = "clientDomain">
+                        <img class="logo-img" :src="require('@/assets/img/gallery.png')"/> Gallery</a>
+                    <!--<button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#tmNavbar">
+                        &#9776;
+                    </button>
+                    <div class="collapse navbar-toggleable-md text-xs-center text-uppercase tm-navbar" id="tmNavbar">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item active selected">
+                                <a class="nav-link" href="#0" data-no="1">1st fluid <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#0" data-no="2">2nd fluid</a>
+                            </li>
+                        </ul>
+                    </div>-->
+                </div>
+            </nav>
+        </div>
+        <ul class="cd-hero-slider">
+            <!-- Page 1 Gallery One -->
+            <li class="selected">
+                <div class="cd-full-width">
+                    <div class="container-fluid js-tm-page-content" data-page-no="1" data-page-type="gallery">
+                        <div class="tm-img-gallery-container">
+                            <div class="tm-img-gallery gallery-one">
+                                <!-- Gallery One pop up connected with JS code below -->
+                                <div v-for="file in files" :key="file.id" class="grid-item">
+                                    <figure class="effect-sadie">
+                                        <img v-bind:src = file.fileUriCrop alt="Image" class="img-fluid tm-img" >
+                                        <figcaption>
+                                            <h2 class="tm-figure-title"><span><strong></strong></span></h2>
+                                            <p class="tm-figure-description"></p>
+                                            <a v-bind:href = file.fileUriOriginal>View more</a>
+                                        </figcaption>
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+        <footer class="tm-footer">
+            <div class="tm-social-icons-container text-xs-center">
+                <a href="#" class="tm-social-link"><i class="fa fa-facebook"></i></a>
+                <a href="mailto:quoc.cuong.yb.dhtb@gmail.com" class="tm-social-link"><i class="fa fa-google"></i></a>
+                <a href="#" class="tm-social-link"><i class="fa fa-twitter"></i></a>
+                <a href="https://github.com/QuocCuong0701" target="_blank" class="tm-social-link"><i class="fa fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/pqcuong" target="_blank" class="tm-social-link"><i class="fa fa-linkedin"></i></a>
+            </div>
+            <p class="tm-copyright-text">Made with
+                <img alt="heart" height="20" width="20"
+                     src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
+                by <a href="https://github.com/QuocCuong0701" target="_blank">Quoc Cuong</a></p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    import $ from 'jquery';
+    import 'jquery';
+    import 'bootstrap';
+    import 'magnific-popup';
+    import 'hero-slider';
+    import 'tether';
+    import google from 'google-maps';
+
+    import axios from "axios";
+
+    const serverDomain = "https://galleryservice.herokuapp.com/";
+
+    export default {
+        name: "Index",
+        rules: {
+            "vue/no-multiple-template-root": "off"
+        },
+        data() {
+            return {
+                files: [],
+                errors: [],
+                clientDomain: "https://gift-4-you.herokuapp.com/"
+            }
+        },
+        created() {
+            axios.get(`${serverDomain}/`)
+                .then(res => {
+                    this.files = res.data.data;
+                })
+                .catch(e => {
+                    this.errors.push(e);
+                })
+        }
+    }
+
+    var tm_gray_site = false;
+    if (tm_gray_site) {
+        $('html').addClass('gray');
+    } else {
+        $('html').removeClass('gray');
+    }
+
+    function adjustHeightOfPage(pageNo) {
+
+        var pageContentHeight = 0;
+
+        var pageType = $('div[data-page-no="' + pageNo + '"]').data("page-type");
+
+        if (pageType !== undefined && pageType === "gallery") {
+            pageContentHeight = $(".cd-hero-slider li:nth-of-type(" + pageNo + ") .tm-img-gallery-container").height();
+        } else {
+            pageContentHeight = $(".cd-hero-slider li:nth-of-type(" + pageNo + ") .js-tm-page-content").height() + 20;
+        }
+
+        // Get the page height
+        var totalPageHeight = $('.cd-slider-nav').height() + pageContentHeight + $('.tm-footer').outerHeight();
+
+        // Adjust layout based on page height and window height
+        if (totalPageHeight > $(window).height()) {
+            $('.cd-hero-slider').addClass('small-screen');
+            $('.cd-hero-slider li:nth-of-type(' + pageNo + ')').css("min-height", totalPageHeight + "px");
+        } else {
+            $('.cd-hero-slider').removeClass('small-screen');
+            $('.cd-hero-slider li:nth-of-type(' + pageNo + ')').css("min-height", "100%");
+        }
+    }
+
+    /*
+        Everything is loaded including images.
+    */
+    // $(window).load(function () {
+    $(window).on('load', function () {
+
+        adjustHeightOfPage(1); // Adjust page height
+
+        /* Gallery One pop up
+        -----------------------------------------*/
+        // $(document).ready(function ($) {
+        $('.gallery-one').magnificPopup({
+            delegate: 'a', // child items selector, by clicking on it popup will open
+            type: 'image',
+            gallery: {enabled: true}
+        });
+        // });
+
+        /* Gallery Two pop up
+        -----------------------------------------*/
+        /*$(document).ready(function ($) {
+        $('.gallery-two').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {enabled: true}
+        });
+        });*/
+
+        /* Gallery Three pop up
+        -----------------------------------------*/
+        /*$(document).ready(function ($) {
+        $('.gallery-three').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {enabled: true}
+        });
+        });*/
+
+        /* Collapse menu after click
+        -----------------------------------------*/
+        $('#tmNavbar a').click(function () {
+            $('#tmNavbar').collapse('hide');
+            adjustHeightOfPage($(this).data("no")); // Adjust page height
+        });
+
+        /* Browser resized
+        -----------------------------------------*/
+        $(window).resize(function () {
+            var currentPageNo = $(".cd-hero-slider li.selected .js-tm-page-content").data("page-no");
+
+            // wait 3 seconds
+            setTimeout(function () {
+                adjustHeightOfPage(currentPageNo);
+            }, 1000);
+
+        });
+
+        // Remove preloader (https://ihatetomatoes.net/create-custom-preloading-screen/)
+        $('body').addClass('loaded');
+
+        // Write current year in copyright text.
+        $(".tm-copyright-year").text(new Date().getFullYear());
+
+    });
+
+    /* Google map
+    ------------------------------------------------*/
+    var map = '';
+    var center;
+
+    // eslint-disable-next-line no-unused-vars
+    function initialize() {
+        var mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(37.779724, -122.452152),
+            scrollwheel: false
+        };
+
+        map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+
+        google.maps.event.addDomListener(map, 'idle', function () {
+            calculateCenter();
+        });
+
+        google.maps.event.addDomListener(window, 'resize', function () {
+            map.setCenter(center);
+        });
+    }
+
+    function calculateCenter() {
+        center = map.getCenter();
+    }
+
+    /*function loadGoogleMap() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+        document.body.appendChild(script);
+    }*/
+
+    // DOM is ready
+    /*$(function () {
+        loadGoogleMap(); // Google Map
+    });*/
+</script>
+
+<style scoped>
+
+</style>
